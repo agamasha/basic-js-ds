200fe1e7daddfc452a2db96781c4c8340712d2ca
agamasha <mariaaga17@gmail.com>
2023-12-25 16:27:52 +0300

    stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.storage = {};
+    this.size = 0;
+  }
+  push(element) {
+    this.size += 1;
+    this.storage[this.size] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let remove = this.storage[this.size];
+    delete this.storage[this.size];
+    this.size -= 1;
+    return remove;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.storage[this.size];
   }
 }
 
